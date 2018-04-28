4554aef096a645d17537a9efc14f4fad33ff9dcf
Marco Vito Moscaritolo <mavimo@gmail.com>
2018-04-28 20:13:34 +0200

    refs #67: add test to validate ObjectHydrateHelperTrait

---
+++ tests/LooplineSystems/CloseIoApiWrapper/Library/Fake/ObjectHydrateHelperDemo.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Close.io Api Wrapper - LLS Internet GmbH - Loopline Systems
+ *
+ * @link      https://github.com/loopline-systems/closeio-api-wrapper for the canonical source repository
+ * @copyright Copyright (c) 2014 LLS Internet GmbH - Loopline Systems (http://www.loopline-systems.com)
+ * @license   https://github.com/loopline-systems/closeio-api-wrapper/blob/master/LICENSE (MIT Licence)
+ */
+
+namespace Tests\LooplineSystems\CloseIoApiWrapper\Library\Fake;
+
+use LooplineSystems\CloseIoApiWrapper\CloseIoRequest;
+use LooplineSystems\CloseIoApiWrapper\CloseIoApiWrapper;
+use LooplineSystems\CloseIoApiWrapper\CloseIoConfig;
+use LooplineSystems\CloseIoApiWrapper\Library\ObjectHydrateHelperTrait;
+
+class ObjectHydrateHelperDemo
+{
+    use ObjectHydrateHelperTrait;
+
+    private $first;
+
+    private $second;
+
+    /**
+     * @return mixed
+     */
+    public function getFirst()
+    {
+        return $this->first;
+    }
+
+    /**
+     * @param mixed $first
+     */
+    public function setFirst($first)
+    {
+        $this->first = $first;
+    }
+    /**
+     * @return mixed
+     */
+    public function getSecond()
+    {
+        return $this->second;
+    }
+
+    /**
+     * @param mixed $second
+     */
+    public function setSecond($second)
+    {
+        $this->second = $second;
+    }
+}
